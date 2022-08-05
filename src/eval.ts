39e59e27bbcc0ab1480aaed3f55115b4b12bc584
Jonathon Wilson <chilkari@gmail.com>
2022-08-05 11:19:02 -0600

    Some test refactoring, allowing table-based skips

--- src/eval.ts
+++ src/eval.ts
@@ -1,8 +1,13 @@
-// The simplest (and probably best) solution.
-// Given that the expressions are stated to all
-// be valid, we can simply and safely use the 
-// language's built-in expression evaluator
+// The simplest (and probably best) solution.  Given that the expressions are
+// stated to all be valid, we can simply and safely use the language's built-in
+// expression evaluator
+//
+// How to handle non-integer division is unspecified in the original instructions,
+// so this more correct/powerful evaluator can give slightly different answers to
+// implementations that assume integer division as we evaluate.
 export function evaluate(expr: string):number {
     const result = eval(expr);
+    // One "simulation" of integer division - floor at the end.  This gives
+    // slightly different results for some tests! (and fails)
     return Math.floor(parseInt(result, 10));
 }
--- tests/eval.test.ts
+++ tests/eval.test.ts
@@ -1,14 +1,12 @@
 import { evaluate } from '../src/eval';
 import { basic_tests } from './test-expressions';
 
-// These tests cover the original instructions:
-// * Assumptions:
-// * - The string given is always a valid expression
-// * - You should respect the presedence of the mathematical operations
-// * - The string will only contain numbers and the characters '+-/*'
-
 describe('eval-based expression evaluator', () => {
   basic_tests.forEach(t => {
+    if (t.skipFor.includes('eval')) {
+      test.skip(t.description, () => {})
+      return true;
+    }
     test(t.description, () => {
       const result = evaluate(t.expression)
       expect(result).toBe(t.answer)
